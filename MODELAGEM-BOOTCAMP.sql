312c0f864003cee486db294659b86cb4c98698e9
eritools <ericasrafael@gmail.com>
2022-09-24 16:56:01 -0300

    projeto logico os

--- MODELAGEM-BOOTCAMP.sql
+++ MODELAGEM-BOOTCAMP.sql
@@ -1,3 +1,97 @@
+create database if not exists company;
+
+show databases;
+
+use company;
+show tables;
+
+select * from information_schema.table_constraints
+	where constraint_schema = 'company';
+
+create table employee (
+	fname varchar(20) not null,
+    minit char,
+    lname varchar(20) not null,
+    ssn char(9) not null,
+    birth_date date,
+    address varchar(50),
+    sex char,
+    salary decimal(10,2),
+    super_ssn char(9),
+    dno int not null,
+    constraint check_salary_employee check (salary > 2000.0),
+    primary key (ssn)
+);
+
+-- adicionando chave estrangeira a employee referente aos supervisores de cada funcionário
+-- obs: os empregados que são supervisores não possuem supervisores neste contexto, ou seja, super_ssn é nulo para eles
+
+alter table employee 
+	add constraint fk_employee_super_ssn 
+	foreign key(super_ssn) references employee(ssn)
+    on delete set null
+    on update cascade;
+    
+alter table employee modify dno int not null default 1;
+
+desc employee;
+
+create table department (
+	dname varchar(20) not null unique,
+    dnumber int not null,
+    mgr_ssn char(9) not null,
+    mgr_start_date date not null,
+    dept_create_date date not null,
+    constraint check_create_date check (dept_create_date < mgr_start_date),
+    primary key (dnumber),
+    constraint fk_mgr_ssn foreign key (mgr_ssn) references employee(ssn)
+);
+
+desc department;
+
+create table dept_locations (
+	dnumber int not null,
+    dlocation varchar(20) not null,
+    primary key (dnumber,dlocation),
+    constraint fk_dept_locations foreign key (dnumber) references department(dnumber)
+);
+
+desc dept_locations;
+
+create table project (
+	pname varchar(20) not null unique,
+    pnumber int not null,
+    plocation varchar(20),
+    dnum int not null,
+    primary key (pnumber),
+    constraint fk_project_dept foreign key (dnum) references department(dnumber)
+);
+
+desc project;
+
+create table works_on (
+	essn char(9) not null,
+    pno int not null,
+    hours decimal(3,1) not null,
+    primary key(essn,pno),
+    constraint fk_employee foreign key (essn) references employee(ssn),
+    constraint fk_project foreign key (pno) references project(pnumber)
+);
+
+desc works_on;
+
+create table dependent (
+	essn char(9) not null,
+	dname varchar(20) not null,
+    sex char,
+    birth_date date,
+    relationship varchar(10),
+    primary key (essn, dname),
+    constraint fk_dependent foreign key (essn) references employee(ssn)
+);
+
+desc dependent;
+
 use company;
 show tables;
 
@@ -173,5 +267,87 @@ from employee
 where (salary between 20000 and 40000);
 
 
+-- SUBQUERIES 
 
+use company;
+show tables;
+
+-- projetos em que Smith trabalha
+select pno, hours 
+from works_on, employee
+where essn = ssn and lname='Smith';
+
+-- projetos em que Smith gerencia
+select pnumber 
+from project, department, employee
+where dnum=dnumber and mgr_ssn = ssn and lname='Smith';
+
+select * from employee;
+
+-- retornar todos os projetos que Smith trabalha e gerencia
+select distinct pnumber 
+from project where pnumber in 
+		(select pnumber from project, department, employee
+        where dnum =dnumber and mgr_ssn = ssn and lname='Smith')
+        or 
+        pnumber in
+        (select pno from works_on, employee
+        where essn = ssn and lname='Smith');
+        
+-- comparação com apenas um atributo
+select distinct essn from works_on 
+where (pno,hours) in (select pno, hours from works_on where essn = '123456789');
+
+-- recuperando os maiores salários se comparados ao departamento 5
+select lname, fname, salary 
+from employee
+where salary > all (select salary from employee where dno=5); -- retorna TRUE se TODOS os valores da subconsulta atenderem à condição
+
+select lname, fname, salary 
+from employee
+where salary = any (select salary from employee where dno=5); -- retorna TRUE se QUALQUER um dos valores da subconsulta atender à condição (dno=5)
+
+select lname, fname, salary 
+from employee
+where salary < all (select salary from employee where dno=5);  -- retorna TRUE se TODOS os valores da subconsulta atenderem à condição
+
+select * from employee where dno=5;
+
+-- empregados que possuem dependentes
+select e.fname, e.lname 
+from employee as e
+where exists (	select * from dependent as d 
+				where e.ssn=d.essn and e.sex = d.sex ); -- verificando se id do employee está em dependent
+                
+-- recuperando nome de empregados que não possuem dependentes
+select concat(e.fname,' ', e.lname) as Employee
+	from employee as e
+	where not exists (	select * from dependent as d 
+						where e.ssn=d.essn
+					  ); 
+                      
+-- gerentes com dependentes
+select e.fname, e.lname 
+	from employee as e
+	where exists (	select * from dependent as d 
+                    where e.ssn=d.essn
+				 )
+                 and
+                 exists (select * from department
+                 where ssn = mgr_ssn); 
+                 
+-- acima de um n° de dependentes
+select e.fname, e.lname 
+	from employee as e
+	where exists (	select * from dependent as d 
+                    where e.ssn=d.essn
+				 )>=2; 
+                 
+-- recuperando o ssn de todos empregados que trabalham nos projetos 1,2 ou 3
+select distinct Essn from works_on where Pno in (1,2,3);
 						
+
+
+
+
+
---
+++ PROJETO LÓGICO OFICINA.sql
@@ -0,0 +1,239 @@
+--
+-- PROJETO LÓGICO DE BANCO DE DADOS ( ORDEM DE SERVIÇO DE OFOCINA MECÂNICA )
+--
+
+create database if not exists workshop;
+use workshop;
+
+create table cliente(
+	idCliente int auto_increment primary key,
+    nome varchar(20),
+    sobrenome varchar(20),
+    rua varchar(30),
+    numero int,
+    bairro varchar(20),
+    cidade varchar(20),
+    UF char(2)
+);
+
+alter table cliente auto_increment=1;
+
+insert into cliente 
+(nome, sobrenome, rua, numero, bairro, cidade, UF) values
+('Maria','Silva','Rua Silva de Prata',29,'Cangola','Cidade das Flores','RJ'),
+('Matheus','Pimentel','Rua Almeida',289,'Centro','Cidade das Flores','RJ'),
+('Ricardo','Silva','Avenida Almeida Vinha',1009,'Centro','Cidade das Flores','RJ'),
+('Julia','França','Rua Lareiras',861,'Centro','Cidade das Flores','RJ'),
+('Roberta','Assis','Avenida Koller',19,'Centro','Cidade das Flores','RJ'),
+('Isabela','Cruz','Rua Almeida das Flores',28,'Cangola','Cidade das Flores','RJ'),
+('Coca Cola','LTDA','Rua Estação Central',111,'Central','São Paulo','SP'),
+('Apple Computer','LTDA','Rua Porto Seguro',89,'Rainha Isabela II','São Paulo','SP');
+
+create table cliente_fisico(
+    CPF char(11) not null,
+    idCliente int,
+    constraint fk_cpf_client foreign key (idCliente) references cliente(idCliente),
+    constraint unique_cpf_client unique(CPF)
+);
+
+select * from cliente; -- somente id 7 e 8 clientes CNPJ
+
+insert into cliente_fisico 
+(idCliente, CPF) values
+(1,'123456789'),
+(2,'987654321'),
+(3,'456789130'),
+(4,'789123456'),
+(5,'987456310'),
+(6,'654789123');
+
+
+create table cliente_juridico(
+    CNPJ char(15) not null,
+    razao_social varchar(45),
+    idCliente int,
+    constraint fk_cnpj_client foreign key (idCliente) references cliente(idCliente),
+    constraint unique_cnpj_client unique(CNPJ)
+);
+
+insert into cliente_juridico(idCliente, CNPJ, razao_social) values
+(7,'45997418000153','Coca Cola Indústrias Ltda'),
+(8,'65478912300000','Apple Brasil');
+
+-- tabela contatos_cliente = atributo multivalorado de cliente
+-- cliente pode ter mais de um contato, e um contato pode ser cadastrado mais de uma vez, porém para clientes distintos
+
+create table contatos_cliente(
+    telefone varchar(45) not null,
+    idCliente int, 
+    primary key(idCliente,telefone),
+    constraint fk_contato_client foreign key (idCliente) references cliente(idCliente)
+);
+
+insert into contatos_cliente (telefone , idCliente ) values
+('987654789',1),
+('987654789',2),
+('987656789',3),
+('998754789',4),
+('987654620',5),
+('987735789',6),
+('987658369',7),
+('987657359',8),
+('987654790',1),
+('987600090',5);
+
+create table mecanico(
+	idMecanico int primary key auto_increment,
+    nome varchar(45),
+    rua varchar(45),
+    numero int,
+    bairro varchar(45),
+    cidade varchar(45),
+    UF char(2),
+    especialidade varchar(45)
+);
+
+insert into mecanico 
+(nome, rua, numero, bairro, cidade, UF, especialidade ) values
+('Mauro Ribeiro','Rua Silva',100,'Cangola','Cidade das Flores','RJ','Calibrador'),
+('Jose Neto Assis','Rua Jojo de Ouro',367,'Centro','Cidade das Flores','RJ','Checa Motor'),
+('Marcos Andrade','Avenida Pinhozinho',7485,'Centro','Cidade das Flores','RJ','Troca Peças'),
+('Romulo Lopes Santos','Rua Limoeiro',825,'Centro','Cidade das Flores','RJ','Troca Óleo'),
+('Francisco Filho','Avenida Santos Dummonteiro',888,'Centro','Cidade das Flores','RJ','Manutenção Diária'),
+('Sanzio Ferreira','Rua Ricardo San Lima',878,'Cangola','Cidade das Flores','RJ','Pintor de Veículos'),
+('Breno Marcos Sousa','Rua Estação Central',222,'Central','São Paulo','SP','Revião Geral'),
+('Felipe Lima Matos','Rua Potira',890,'Rainha Isabela II','São Paulo','SP','Conserto Geral');
+
+create table peça(
+	idPeça int primary key auto_increment,
+    nome varchar(45) not null unique,
+    valor_da_peça float
+);
+
+insert into peça (nome, valor_da_peça ) values
+('Velas de ignição',50.90),
+('Filtro de óleo',87.65),
+('Pistão',30.50),
+('Caixa de câmbio',300),
+('Bateria de moto',400),
+('Pastilhas dos freios',150.0),
+('Embreagem',150.0),
+('Correntes',70.50),
+('Pneus',200);
+
+create table veiculo(
+	idVeiculo int primary key auto_increment,
+    modelo varchar(45),
+    placa varchar(45),
+    idCliente int,
+    idMecanico int,
+    constraint fk_veiculo_cliente foreign key (idCliente) references cliente(idCliente),
+    constraint fk_veiculo_mecanico foreign key (idMecanico) references mecanico(idMecanico)
+);
+
+insert into veiculo (modelo, placa, idCliente, idMecanico) values
+('Renault Logan Life 1.0 flex manual 5 marchas','BR2E193',3,1),
+('Honda City Hatch EXL 1.5 flex CVT','BRA0176',2,3),
+('Honda City EX 1.5 flex CVT','OTM2022',4,5),
+('Fiat Mobi Like 1.0 flex manual 5 marchas','BRA7S19',3,7),
+('Fiat Cronos 1.3 flex manual 5 marchas','HQW5678',5,4),
+('Volkswagen Gol 1.0 flex manual 5 marchas','GFZ0011',1,6),
+('Honda Biz 110','ABC1234',1,7),
+('Honda Pop 110','MMM0000',6,8),
+('Scania R500','ERM9864',7,7),
+('Volvo FH 540','UIO82563',8,8);
+
+create table os(
+	idOs int primary key auto_increment,
+    data_emissao varchar(45),
+    valor_total_estimado varchar(45),
+    data_conclusao varchar(45),
+    status_serviço enum('Não Iniciado','Em Andamento','Cancelado pelo Cliente','Concluído') default 'Em Andamento' ,
+	idVeiculo int,
+    constraint fk_os_veiculo foreign key (idVeiculo) references veiculo(idVeiculo),
+    constraint data_conlusao_os check (data_conclusao >= data_emissao)
+);
+
+insert into os (data_emissao, valor_total_estimado, data_conclusao, status_serviço, idVeiculo ) values
+('2022-02-01',250.0,'2022-02-02','Concluído', 1),
+('2022-09-23',300,'2022-09-25',default, 4),
+('2022-03-01',100,'2022-03-02','Concluído', 2),
+('2022-04-01',150.80,'2022-04-02','Cancelado pelo Cliente', 3),
+('2022-05-01',345.98,'2022-05-02','Cancelado pelo Cliente', 4),
+('2022-05-01',100.50,'2022-05-02','Concluído', 6),
+('2022-09-23',250.0,'2022-09-25',default, 5),
+('2022-07-01',145.50,'2022-07-02','Concluído', 7),
+('2022-02-01',200,'2022-02-02','Concluído', 8),
+('2022-08-01',100,'2022-08-02','Concluído', 7),
+('2022-09-24',500,'2022-09-27','Não Iniciado', 9),
+('2022-09-23',400,'2022-09-25',default, 10);
+
+create table peças_os(
+	idOs int,
+    idPeça int,
+    quantidade int not null,
+    primary key(idos,idPeça),
+	constraint fk_OP_os foreign key (idOs) references os(idOs),
+    constraint fk_OP_peça foreign key (idPeça) references peça(idPeça)
+);
+
+insert into peças_os (idOs, idPeça, quantidade ) values
+(1,3,1),
+(2,6,1),
+(3,3,1),
+(4,3,2),
+(5,9,4),
+(6,3,2),
+(7,8,1),
+(8,5,1),
+(9,3,2),
+(10,4,1),
+(10,2,2),
+(9,9,4),
+(7,7,1),
+(5,1,1),
+(2,3,1);
+
+-- valores tabelados de cada serviço - manter subclasses
+-- superclasse serviços não tinha atributos generalizados
+
+create table os_revisao(
+	idOs int not null,   -- chave da superclasse
+    descriçao varchar(45) not null,
+	mao_de_obra float not null default 10,
+    primary key(idOs,descriçao),
+    constraint fk_revisao_os foreign key (idOs) references os(idOs)
+);
+
+insert into os_revisao( idOs, descriçao, mao_de_obra ) values 
+(1, 'Troca de Pistão', 20 ),
+(2, 'Troca de Pistão', 20 ),
+(3, 'Colocar Pastilhas de Freios', default),
+(3, 'Troca de Pistão', 10 ),
+(6, 'Troca de Pistão', 20 ),
+(11, 'Troca de Filtro de Oleo', 10 ),
+(12, 'Troca de Pneus', 50 );
+
+create table os_conserto(
+	idOs int,  -- chave da superclasse
+    descriçao varchar(45),
+	mao_de_obra float not null,
+    primary key(idOs,descriçao),
+    constraint fk_conserto_os foreign key (idOS) references os(idOs)
+);
+
+insert into os_revisao( idOs, descriçao, mao_de_obra ) values 
+(10, 'Conserto Filtro de óleo', 60 ),
+(8, 'Conserto Embreagem', 20 ),
+(7, 'Conserto Filtro de óleo', 20 ),
+(8, 'Conserto Freios', 20 ),
+(11, 'Conserto Volante', 50 );
+
+select * from os; -- veiculo
+
+select * from cliente 
+join veiculo using(idCliente) 
+join os using(idVeiculo)
+where status_serviço like '%Cancelado%';
+
+select * from veiculo;
